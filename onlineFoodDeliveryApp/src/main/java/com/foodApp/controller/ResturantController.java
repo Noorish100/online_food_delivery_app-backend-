1b74ddf07a52ac309e767e80d697d2d3c954d59a
Noorish100 <mdnoorishnauman@gmail.com>
2022-06-07 00:51:45 +0530

    d

--- onlineFoodDeliveryApp/src/main/java/com/foodApp/controller/ResturantController.java
+++ onlineFoodDeliveryApp/src/main/java/com/foodApp/controller/ResturantController.java
@@ -1,24 +1,24 @@
-package com.foodApp.controller;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.foodApp.model.Resturant;
-import com.foodApp.service.ResturantService;
-
-@RestController
-public class ResturantController{
-	
-	@Autowired
-	private ResturantService resSer;
-	
-	@PostMapping("/Resturant")
-	public Resturant saveResturant(@RequestBody Resturant res) {
-		
-		Resturant r = resSer.addResturant(res);
-		
-		return r;
-	}
-}
+//package com.foodApp.controller;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import com.foodApp.model.Resturant;
+//import com.foodApp.service.ResturantService;
+//
+//@RestController
+//public class ResturantController{
+//	
+//	@Autowired
+//	private ResturantService resSer;
+//	
+//	@PostMapping("/Resturant")
+//	public Resturant saveResturant(@RequestBody Resturant res) {
+//		
+//		Resturant r = resSer.addResturant(res);
+//		
+//		return r;
+//	}
+//}
--- onlineFoodDeliveryApp/src/main/java/com/foodApp/service/ResturantServiceImpl.java
+++ onlineFoodDeliveryApp/src/main/java/com/foodApp/service/ResturantServiceImpl.java
@@ -1,16 +1,13 @@
 package com.foodApp.service;
 
-import java.lang.StackWalker.Option;
+
 import java.util.List;
 import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.foodApp.model.Resturant;
 import com.foodApp.repository.ResturantDao;
-import com.masai.exception.StudentException;
-import com.masai.model.Student;
+
 
 @Service
 public class ResturantServiceImpl implements ResturantService{
@@ -18,24 +15,20 @@ public class ResturantServiceImpl implements ResturantService{
 	@Autowired
 	private ResturantDao rdao;
 	
-	@Override
-	public Resturant addResturant(Resturant res) {
-		// TODO Auto-generated method stub
-		return rdao.save(res);
-	}
+
 
 	@Override
 	public Resturant updateResturant(Resturant res) {
 		// TODO Auto-generated method stub
 		
-		Optional<Resturant> optR = rdao.findById(res.getResturantId());
-		
-		if(optR.isPresent()) {
-			
-			Resturant existingResturant= optR.get();
-			
-			return rdao.save(res);
-		}
+//		Optional<Resturant> optR = rdao.findById(res.getResturantId());
+//		
+//		if(optR.isPresent()) {
+//			
+//			Resturant existingResturant= optR.get();
+//			
+//			return rdao.save(res);
+//		}
 		return null;
 	}
 	
@@ -60,6 +53,13 @@ public class ResturantServiceImpl implements ResturantService{
 
 	@Override
 	public List<Resturant> viewResturantByItemName(String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public Resturant addResturant(Resturant res) {
 		// TODO Auto-generated method stub
 		return null;
 	}
